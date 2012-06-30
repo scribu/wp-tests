a254af313b332ab4fe95f51b9a4214ee4845e127
maxcutler <maxcutler@4a5a2adb-35eb-4268-9a1b-fe04a4134342>
2012-06-30 20:31:06 +0000

    Port test_import_wp.php. Fixes #62.
    git-svn-id: https://unit-test.svn.wordpress.org/trunk@803 4a5a2adb-35eb-4268-9a1b-fe04a4134342

--- wp-testcase/test_import_wp.php
+++ wp-testcase/test_import_wp.php
@@ -4,9 +4,62 @@
 
 // need to include the importer as an external, so path may need to change...
 
-// _nuke_main_tables style function on tearDown? (Should this be part of _WPEmptyBlog tearDown?)
+abstract class WP_ImportTestCase extends WP_UnitTestCase {
+	/**
+	 * Import a WXR file.
+	 *
+	 * The $users parameter provides information on how users specified in the import
+	 * file should be imported. Each key is a user login name and indicates if the user
+	 * should be mapped to an existing user, created as a new user with a particular login
+	 * or imported with the information held in the WXR file. An example of this:
+	 *
+	 * <code>
+	 * $users = array(
+	 *   'alice' => 1, // alice will be mapped to user ID 1
+	 *   'bob' => 'john', // bob will be transformed into john
+	 *   'eve' => false // eve will be imported as is
+	 * );</code>
+	 *
+	 * @param string $filename Full path of the file to import
+	 * @param array $users User import settings
+	 * @param bool $fetch_files Whether or not do download remote attachments
+	 */
+	protected function _import_wp( $filename, $users = array(), $fetch_files = true ) {
+		$importer = new WP_Import();
+		$file = realpath( $filename );
+		assert('!empty($file)');
+		assert('is_file($file)');
 
-class WXRParserTest extends WPTestCase {
+		$authors = $mapping = array();
+		$i = 0;
+
+		// each user is either mapped to a given ID, mapped to a new user
+		// with given login or imported using details in WXR file
+		foreach ( $users as $user => $map ) {
+			$authors[$i] = $user;
+			if ( is_int( $map ) )
+				$mapping[$i] = $map;
+			else if ( is_string( $map ) )
+				$new[$i] = $map;
+
+			$i++;
+		}
+
+		$_POST = array( 'imported_authors' => $authors, 'user_map' => $mapping, 'user_new' => $new );
+
+		ob_start();
+		$importer->fetch_attachments = $fetch_files;
+		$importer->import( $file );
+		ob_end_clean();
+
+		$_POST = array();
+	}
+}
+
+/**
+ * @group import
+ */
+class WXRParserTest extends WP_ImportTestCase {
 	function setUp() {
 		parent::setUp();
 
@@ -17,10 +70,6 @@ class WXRParserTest extends WPTestCase {
 			define( 'WP_LOAD_IMPORTERS', true );
 
 		require_once DIR_TESTDATA . '/plugins/wordpress-importer/wordpress-importer.php';
-	}
-
-	function tearDown() {
-		parent::tearDown();
 	}
 
 	function test_malformed_wxr() {
@@ -198,7 +247,10 @@ class WXRParserTest extends WPTestCase {
 	// tags in CDATA #11574
 }
 
-class WPImportTest extends _WPEmptyBlog {
+/**
+ * @group import
+ */
+class WPImportTest extends WP_ImportTestCase {
 	function setUp() {
 		parent::setUp();
 
@@ -218,17 +270,9 @@ class WPImportTest extends _WPEmptyBlog {
 	}
 
 	function tearDown() {
-		parent::tearDown();
-
 		remove_filter( 'import_allow_create_users', '__return_true' );
-		$this->_delete_all_posts();
 
-		if ( $user = get_user_by( 'login', 'admin' ) )
-			$this->_destroy_user( $user->ID );
-		if ( $user = get_user_by( 'login', 'editor' ) )
-			$this->_destroy_user( $user->ID );
-		if ( $user = get_user_by( 'login', 'author' ) )
-			$this->_destroy_user( $user->ID );
+		parent::tearDown();
 	}
 
 	function test_small_import() {
@@ -428,7 +472,10 @@ class WPImportTest extends _WPEmptyBlog {
 	// function test_menu_import
 }
 
-class TestImportWP_PostMeta extends _WPEmptyBlog {
+/**
+ * @group import
+ */
+class TestImportWP_PostMeta extends WP_ImportTestCase {
 	function setUp() {
 		parent::setUp();
 
@@ -439,10 +486,6 @@ class TestImportWP_PostMeta extends _WPEmptyBlog {
 			define( 'WP_LOAD_IMPORTERS', true );
 
 		require_once DIR_TESTDATA . '/plugins/wordpress-importer/wordpress-importer.php';
-	}
-
-	function tearDown() {
-		parent::tearDown();
 	}
 
 	function test_serialized_postmeta_no_cdata() {
@@ -499,7 +542,10 @@ class TestImportWP_PostMeta extends _WPEmptyBlog {
 	}
 }
 
-class TestImportWP_PostMetaCDATA extends _WPEmptyBlog {
+/**
+ * @group import
+ */
+class TestImportWP_PostMetaCDATA extends WP_ImportTestCase {
 	function setUp() {
 		parent::setUp();
 
@@ -510,10 +556,6 @@ class TestImportWP_PostMetaCDATA extends _WPEmptyBlog {
 			define( 'WP_LOAD_IMPORTERS', true );
 
 		require_once DIR_TESTDATA . '/plugins/wordpress-importer/wordpress-importer.php';
-	}
-
-	function tearDown() {
-		parent::tearDown();
 	}
 
 	// #9633
@@ -535,5 +577,3 @@ class TestImportWP_PostMetaCDATA extends _WPEmptyBlog {
 		$this->assertEquals( '<wp:meta_value>evil</wp:meta_value>', get_post_meta( 10, 'evil', true ) );
 	}
 }
-
-?>
