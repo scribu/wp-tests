5a38a2e58f77defcf822da1fe5ccfb346f8ad714
nacin <nacin@4a5a2adb-35eb-4268-9a1b-fe04a4134342>
2012-06-30 21:21:38 +0000

    Port test_includes_compat. fixes #76.
    git-svn-id: https://unit-test.svn.wordpress.org/trunk@816 4a5a2adb-35eb-4268-9a1b-fe04a4134342

--- wp-testcase/test_includes_compat.php
+++ wp-testcase/test_includes_compat.php
@@ -1,6 +1,6 @@
 <?php
 
-class TestMbSubStr extends WPTestCase {
+class TestMbSubStr extends WP_UnitTestCase {
 	function test_mb_strcut() {
 		$this->assertEquals('баб', _mb_substr('баба', 0, 3));
 		$this->assertEquals('баб', _mb_substr('баба', 0, -1));
@@ -9,7 +9,7 @@ class TestMbSubStr extends WPTestCase {
 	}
 }
 
-class TestHashHMAC extends WPTestCase {
+class TestHashHMAC extends WP_UnitTestCase {
 	function test_simple() {
 		$this->assertEquals('140d1cb79fa12e2a31f32d35ad0a2723', _hash_hmac('md5', 'simple', 'key'));
 		$this->assertEquals('993003b95758e0ac2eba451a4c5877eb1bb7b92a', _hash_hmac('sha1', 'simple', 'key'));
