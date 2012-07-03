e15c897d1826cf2c6e2288e22460d1aa1f8f84dc
nacin <nacin@4a5a2adb-35eb-4268-9a1b-fe04a4134342>
2012-07-03 17:40:48 +0000

    Set tests memory_limit to -1. Any changes by core use these constants. We can consider adding it to tearDown() at some point in the future. fixes #6.
    git-svn-id: https://unit-test.svn.wordpress.org/trunk@876 4a5a2adb-35eb-4268-9a1b-fe04a4134342

--- bootstrap.php
+++ bootstrap.php
@@ -30,6 +30,9 @@ if ( ! defined( 'WP_TESTS_FORCE_KNOWN_BUGS' ) )
 // Cron tries to make an HTTP request to the blog, which always fails, because tests are run in CLI mode only
 define( 'DISABLE_WP_CRON', true );
 
+define( 'WP_MEMORY_LIMIT', -1 );
+define( 'WP_MAX_MEMORY_LIMIT', -1 );
+
 $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
 $_SERVER['HTTP_HOST'] = WP_TESTS_DOMAIN;
 $PHP_SELF = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
