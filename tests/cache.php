8aead8487b497ce9f366e6f1c30f5ba958283aff
nacin <nacin@4a5a2adb-35eb-4268-9a1b-fe04a4134342>
2012-07-20 20:13:25 +0000

    wp_cache_delete() does not have a $force method. see #110.
    git-svn-id: https://unit-test.svn.wordpress.org/trunk@933 4a5a2adb-35eb-4268-9a1b-fe04a4134342

--- tests/cache.php
+++ tests/cache.php
@@ -215,8 +215,9 @@ class Tests_Cache extends WP_UnitTestCase {
 		$this->assertTrue( wp_cache_delete( $key ) );
 		$this->assertFalse( wp_cache_get( $key ) );
 
+		// wp_cache_delete() does not have a $force method.
 		// Delete returns (bool) true when key is not set and $force is true
-		$this->assertTrue( wp_cache_delete( $key, 'default', true ) );
-		$this->assertFalse( wp_cache_delete( $key, 'default') );
+		// $this->assertTrue( wp_cache_delete( $key, 'default', true ) );
+		// $this->assertFalse( wp_cache_delete( $key, 'default') );
 	}
 }
