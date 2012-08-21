a1796c5e8579dca00b2f3dc40c5ea38ef63a479e
SergeyBiryukov <SergeyBiryukov@4a5a2adb-35eb-4268-9a1b-fe04a4134342>
2012-08-21 10:30:24 +0000

    Use @ticket annotation in comment.php. see [979]. see #102.
    git-svn-id: https://unit-test.svn.wordpress.org/trunk@984 4a5a2adb-35eb-4268-9a1b-fe04a4134342

--- tests/comment.php
+++ tests/comment.php
@@ -14,14 +14,18 @@ class Test_Comment_Query extends WP_UnitTestCase {
 		$this->comment_id = $this->factory->comment->create();
 	}
 
+	/**
+	 * @ticket 21101
+	 */
 	function test_get_comment_comment_approved_0() {
-		$this->knownWPBug( 21101 );
 		$comments_approved_0 = get_comments( array( 'comment_approved' => '0' ) );
 		$this->assertEquals( 0, count( $comments_approved_0 ) );
 	}
 
+	/**
+	 * @ticket 21101
+	 */
 	function test_get_comment_comment_approved_1() {
-		$this->knownWPBug( 21101 );
 		$comments_approved_1 = get_comments( array( 'comment_approved' => '1' ) );
 
 		$this->assertEquals( 1, count( $comments_approved_1 ) );
