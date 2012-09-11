be01ffce99ee6b92d76ba1d35f83b7dd22861d34
nacin <nacin@4a5a2adb-35eb-4268-9a1b-fe04a4134342>
2012-09-11 21:02:10 +0000

    Test that XML-RPC is enabled, not disabled. see #WP21509.
    git-svn-id: https://unit-test.svn.wordpress.org/trunk@1015 4a5a2adb-35eb-4268-9a1b-fe04a4134342

--- tests/xmlrpc/enabled.php
+++ tests/xmlrpc/enabled.php
@@ -7,12 +7,13 @@ require_once ABSPATH . WPINC . '/class-wp-xmlrpc-server.php';
 /**
  * @group xmlrpc
  */
-class Tests_XMLRPC_Disabled extends WP_UnitTestCase {
-	function test_disabled() {
+class Tests_XMLRPC_Enabled extends WP_UnitTestCase {
+	function test_enabled() {
 		$myxmlrpcserver = new wp_xmlrpc_server();
 		$result = $myxmlrpcserver->wp_getOptions( array( 1, 'username', 'password' ) );
 
 		$this->assertInstanceOf( 'IXR_Error', $result );
-		$this->assertEquals( 405, $result->code );
+		// If disabled, 405 would result.
+		$this->assertEquals( 403, $result->code );
 	}
 }
