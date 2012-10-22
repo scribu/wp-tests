faac3726a83f48a25edfffc2e738506820ab12d6
scribu <mail@scribu.net>
2012-10-22 23:17:01 +0300

    add Tests_Actions_Namespaces

---
+++ tests/actions/namespaces.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Test hook namespaces
+ *
+ * @group hooks
+ */
+class Tests_Actions_Namespaces extends WP_UnitTestCase {
+
+	function test_namespaces() {
+		$this->assertFalse( has_filter( 'test' ) );
+
+		add_filter( 'test.p2p', '__return_true' );
+		$this->assertTrue( has_filter( 'test' ) );
+
+		remove_filter( 'test.p2p' );
+		$this->assertFalse( has_filter( 'test' ) );
+	}
+}
+
