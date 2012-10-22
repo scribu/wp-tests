b7adfebb0d4e914572cccbaa0b86f69a4be4bd6b
scribu <mail@scribu.net>
2012-10-22 23:22:18 +0300

    set namespace separator to ':'

--- tests/actions/namespaces.php
+++ tests/actions/namespaces.php
@@ -10,10 +10,10 @@ class Tests_Actions_Namespaces extends WP_UnitTestCase {
 	function test_namespaces() {
 		$this->assertFalse( has_filter( 'test' ) );
 
-		add_filter( 'test.p2p', '__return_true' );
+		add_filter( 'test:p2p', '__return_true' );
 		$this->assertTrue( has_filter( 'test' ) );
 
-		remove_filter( 'test.p2p' );
+		remove_filter( 'test:p2p' );
 		$this->assertFalse( has_filter( 'test' ) );
 	}
 }
