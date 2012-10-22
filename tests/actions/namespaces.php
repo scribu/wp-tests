3ed57dd8f560ed7c30c5a5ad2d4885c01fdff090
scribu <mail@scribu.net>
2012-10-23 00:05:59 +0300

    test for remove_action() with wildcard

--- tests/actions/namespaces.php
+++ tests/actions/namespaces.php
@@ -7,7 +7,7 @@
  */
 class Tests_Actions_Namespaces extends WP_UnitTestCase {
 
-	function test_namespaces() {
+	function test_remove() {
 		$this->assertFalse( has_filter( 'test' ) );
 
 		add_filter( 'test:p2p', '__return_true' );
@@ -15,6 +15,22 @@ class Tests_Actions_Namespaces extends WP_UnitTestCase {
 
 		remove_filter( 'test:p2p' );
 		$this->assertFalse( has_filter( 'test' ) );
+	}
+
+	function test_remove_wildcard() {
+		$this->assertFalse( has_filter( 'foo' ) );
+		$this->assertFalse( has_filter( 'bar' ) );
+
+		add_filter( 'foo:p2p', '__return_true' );
+		$this->assertTrue( has_filter( 'foo' ) );
+
+		add_filter( 'bar:p2p', '__return_true' );
+		$this->assertTrue( has_filter( 'bar' ) );
+
+		remove_filter( '*:p2p' );
+
+		$this->assertFalse( has_filter( 'foo' ) );
+		$this->assertFalse( has_filter( 'bar' ) );
 	}
 }
 
