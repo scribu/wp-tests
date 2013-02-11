8f3f4a3f3eac63b942b110bc0aad9633b0dc6b62
ryan <ryan@4a5a2adb-35eb-4268-9a1b-fe04a4134342>
2013-02-11 18:32:19 +0000

    Update cache tests to reflect new last_changed value. See #WP23448
    git-svn-id: https://unit-test.svn.wordpress.org/trunk@1209 4a5a2adb-35eb-4268-9a1b-fe04a4134342

--- tests/term/getTerms.php
+++ tests/term/getTerms.php
@@ -19,16 +19,16 @@ class Tests_Term_getTerms extends WP_UnitTestCase {
 		$posts = $this->factory->post->create_many( 15, array( 'post_type' => 'post' ) );
 		foreach ( $posts as $post )
 			wp_set_object_terms( $post, rand_str(), 'post_tag' );
+		wp_cache_delete( 'last_changed', 'terms' );
 
 		$this->assertFalse( wp_cache_get( 'last_changed', 'terms' ) );
 
 		$num_queries = $wpdb->num_queries;
-		$last_changed = 1;
 
 		// last_changed and num_queries should bump
 		$terms = get_terms( 'post_tag' );
 		$this->assertEquals( 15, count( $terms ) );
-		$this->assertEquals( 1, wp_cache_get( 'last_changed', 'terms' ) );
+		$this->assertNotEmpty( $time1 = wp_cache_get( 'last_changed', 'terms' ) );
 		$this->assertEquals( $num_queries + 1, $wpdb->num_queries );
 
 		$num_queries = $wpdb->num_queries;
@@ -36,7 +36,7 @@ class Tests_Term_getTerms extends WP_UnitTestCase {
 		// Again. last_changed and num_queries should remain the same.
 		$terms = get_terms( 'post_tag' );
 		$this->assertEquals( 15, count( $terms ) );
-		$this->assertEquals( $last_changed, wp_cache_get( 'last_changed', 'terms' ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'terms' ) );
 		$this->assertEquals( $num_queries, $wpdb->num_queries );
 
 		$num_queries = $wpdb->num_queries;
@@ -45,7 +45,7 @@ class Tests_Term_getTerms extends WP_UnitTestCase {
 		// Different query. num_queries should bump, last_changed should remain the same.
 		$terms = get_terms( 'post_tag', array( 'number' => 10 ) );
 		$this->assertEquals( 10, count( $terms ) );
-		$this->assertEquals( $last_changed, wp_cache_get( 'last_changed', 'terms' ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'terms' ) );
 		$this->assertEquals( $num_queries + 1, $wpdb->num_queries );
 
 		$num_queries = $wpdb->num_queries;
@@ -53,20 +53,19 @@ class Tests_Term_getTerms extends WP_UnitTestCase {
 		// Again. last_changed and num_queries should remain the same.
 		$terms = get_terms( 'post_tag', array( 'number' => 10 ) );
 		$this->assertEquals( 10, count( $terms ) );
-		$this->assertEquals( $last_changed, wp_cache_get( 'last_changed', 'terms' ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'terms' ) );
 		$this->assertEquals( $num_queries, $wpdb->num_queries );
 
 		// Force last_changed to bump
 		wp_delete_term( $terms[0]->term_id, 'post_tag' );
 
 		$num_queries = $wpdb->num_queries;
-		// Bump by two because both wp_delete_term() and wp_update_term_count_now() call clean_term_cache().
-		$last_changed += 2;
+		$this->assertNotEquals( $time1, $time2 = wp_cache_get( 'last_changed', 'terms' ) );
 
 		// last_changed and num_queries should bump after a term is deleted
 		$terms = get_terms( 'post_tag' );
 		$this->assertEquals( 14, count( $terms ) );
-		$this->assertEquals( $last_changed, wp_cache_get( 'last_changed', 'terms' ) );
+		$this->assertEquals( $time2, wp_cache_get( 'last_changed', 'terms' ) );
 		$this->assertEquals( $num_queries + 1, $wpdb->num_queries );
 
 		$num_queries = $wpdb->num_queries;
@@ -74,7 +73,7 @@ class Tests_Term_getTerms extends WP_UnitTestCase {
 		// Again. last_changed and num_queries should remain the same.
 		$terms = get_terms( 'post_tag' );
 		$this->assertEquals( 14, count( $terms ) );
-		$this->assertEquals( $last_changed, wp_cache_get( 'last_changed', 'terms' ) );
+		$this->assertEquals( $time2, wp_cache_get( 'last_changed', 'terms' ) );
 		$this->assertEquals( $num_queries, $wpdb->num_queries );
 
 		// @todo Repeat with term insert and update.
