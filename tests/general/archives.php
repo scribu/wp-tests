1fed5b004b004d7d7c8bec78bc3a2e320f5b4e64
ryan <ryan@4a5a2adb-35eb-4268-9a1b-fe04a4134342>
2013-02-11 18:21:59 +0000

    Update cache tests to reflect new last_changed value. See #WP23448
    git-svn-id: https://unit-test.svn.wordpress.org/trunk@1206 4a5a2adb-35eb-4268-9a1b-fe04a4134342

--- tests/general/archives.php
+++ tests/general/archives.php
@@ -17,6 +17,7 @@ class Tests_General_Archives extends WP_UnitTestCase {
 		global $wpdb;
 
 		$this->factory->post->create_many( 15, array( 'post_type' => 'post' ) );
+		wp_cache_delete( 'last_changed', 'posts' );
 		$this->assertFalse( wp_cache_get( 'last_changed', 'posts' ) );
 
 		$num_queries = $wpdb->num_queries;
@@ -24,7 +25,7 @@ class Tests_General_Archives extends WP_UnitTestCase {
 		// Cache is not primed, expect 1 query.
 		$result = wp_get_archives( array( 'type' => 'monthly', 'echo' => false ) );
 		$this->assertInternalType( 'string', $result );
-		$this->assertEquals( 1, wp_cache_get( 'last_changed', 'posts' ) );
+		$this->assertNotEmpty( $time1 = wp_cache_get( 'last_changed', 'posts' ) );
 		$this->assertEquals( $num_queries + 1, $wpdb->num_queries );
 
 		$num_queries = $wpdb->num_queries;
@@ -32,13 +33,13 @@ class Tests_General_Archives extends WP_UnitTestCase {
 		// Cache is primed, expect no queries.
 		$result = wp_get_archives( array( 'type' => 'monthly', 'echo' => false ) );
 		$this->assertInternalType( 'string', $result );
-		$this->assertEquals( 1, wp_cache_get( 'last_changed', 'posts' ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'posts' ) );
 		$this->assertEquals( $num_queries, $wpdb->num_queries );
 
 		// Change args, resulting in a different query string. Cache is not primed, expect 1 query.
 		$result = wp_get_archives( array( 'type' => 'monthly', 'echo' => false, 'order' => 'ASC' ) );
 		$this->assertInternalType( 'string', $result );
-		$this->assertEquals( 1, wp_cache_get( 'last_changed', 'posts' ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'posts' ) );
 		$this->assertEquals( $num_queries + 1, $wpdb->num_queries );
 
 		$num_queries = $wpdb->num_queries;
@@ -46,7 +47,7 @@ class Tests_General_Archives extends WP_UnitTestCase {
 		// Cache is primed, expect no queries.
 		$result = wp_get_archives( array( 'type' => 'monthly', 'echo' => false, 'order' => 'ASC' ) );
 		$this->assertInternalType( 'string', $result );
-		$this->assertEquals( 1, wp_cache_get( 'last_changed', 'posts' ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'posts' ) );
 		$this->assertEquals( $num_queries, $wpdb->num_queries );
 
 		$num_queries = $wpdb->num_queries;
@@ -54,7 +55,7 @@ class Tests_General_Archives extends WP_UnitTestCase {
 		// Change type. Cache is not primed, expect 1 query.
 		$result = wp_get_archives( array( 'type' => 'yearly', 'echo' => false ) );
 		$this->assertInternalType( 'string', $result );
-		$this->assertEquals( 1, wp_cache_get( 'last_changed', 'posts' ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'posts' ) );
 		$this->assertEquals( $num_queries + 1, $wpdb->num_queries );
 
 		$num_queries = $wpdb->num_queries;
@@ -62,13 +63,13 @@ class Tests_General_Archives extends WP_UnitTestCase {
 		// Cache is primed, expect no queries.
 		$result = wp_get_archives( array( 'type' => 'yearly', 'echo' => false ) );
 		$this->assertInternalType( 'string', $result );
-		$this->assertEquals( 1, wp_cache_get( 'last_changed', 'posts' ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'posts' ) );
 		$this->assertEquals( $num_queries, $wpdb->num_queries );
 
 		// Change type. Cache is not primed, expect 1 query.
 		$result = wp_get_archives( array( 'type' => 'daily', 'echo' => false ) );
 		$this->assertInternalType( 'string', $result );
-		$this->assertEquals( 1, wp_cache_get( 'last_changed', 'posts' ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'posts' ) );
 		$this->assertEquals( $num_queries + 1, $wpdb->num_queries );
 
 		$num_queries = $wpdb->num_queries;
@@ -76,13 +77,13 @@ class Tests_General_Archives extends WP_UnitTestCase {
 		// Cache is primed, expect no queries.
 		$result = wp_get_archives( array( 'type' => 'daily', 'echo' => false ) );
 		$this->assertInternalType( 'string', $result );
-		$this->assertEquals( 1, wp_cache_get( 'last_changed', 'posts' ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'posts' ) );
 		$this->assertEquals( $num_queries, $wpdb->num_queries );
 
 		// Change type. Cache is not primed, expect 1 query.
 		$result = wp_get_archives( array( 'type' => 'weekly', 'echo' => false ) );
 		$this->assertInternalType( 'string', $result );
-		$this->assertEquals( 1, wp_cache_get( 'last_changed', 'posts' ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'posts' ) );
 		$this->assertEquals( $num_queries + 1, $wpdb->num_queries );
 
 		$num_queries = $wpdb->num_queries;
@@ -90,13 +91,13 @@ class Tests_General_Archives extends WP_UnitTestCase {
 		// Cache is primed, expect no queries.
 		$result = wp_get_archives( array( 'type' => 'weekly', 'echo' => false ) );
 		$this->assertInternalType( 'string', $result );
-		$this->assertEquals( 1, wp_cache_get( 'last_changed', 'posts' ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'posts' ) );
 		$this->assertEquals( $num_queries, $wpdb->num_queries );
 
 		// Change type. Cache is not primed, expect 1 query.
 		$result = wp_get_archives( array( 'type' => 'postbypost', 'echo' => false ) );
 		$this->assertInternalType( 'string', $result );
-		$this->assertEquals( 1, wp_cache_get( 'last_changed', 'posts' ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'posts' ) );
 		$this->assertEquals( $num_queries + 1, $wpdb->num_queries );
 
 		$num_queries = $wpdb->num_queries;
@@ -104,7 +105,7 @@ class Tests_General_Archives extends WP_UnitTestCase {
 		// Cache is primed, expect no queries.
 		$result = wp_get_archives( array( 'type' => 'postbypost', 'echo' => false ) );
 		$this->assertInternalType( 'string', $result );
-		$this->assertEquals( 1, wp_cache_get( 'last_changed', 'posts' ) );
+		$this->assertEquals( $time1, wp_cache_get( 'last_changed', 'posts' ) );
 		$this->assertEquals( $num_queries, $wpdb->num_queries );
 	}
 }
